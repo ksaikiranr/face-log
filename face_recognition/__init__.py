9bebcbb9255f951d6f3789a1a1ba0c5c751505f8
Sai Kiran <sk@localhost.localdomain>
2018-02-16 19:47:56 +0530

    face recognition from web-cam, given sample images from current directory

---
+++ face_recognition/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+__author__ = """K Sai Kiran"""
+__email__ = 'ksaikiranr@gmail.com'
+__version__ = '0.0.1'
+
+from .api import load_image_file, face_locations, batch_face_locations, face_landmarks, face_encodings, compare_faces, face_distance
