988919d93b98fabfc7be5c1b362258ee6e4d54e7
K Sai Kiran <ksaikiranr@gmail.com>
2018-06-13 08:04:02 +0530

    Final demo ready

--- main.py
+++ main.py
@@ -10,11 +10,14 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.screenmanager import ScreenManager, SwapTransition, Screen
 from kivy.base import EventLoop
 ### Kivy imports end ###
+import kivy
+kivy.require("1.10.0")
 
 import os
 import cv2
 import face_recognition
 
+from kivymd import  *
 
 ## UI imports ##
 from kivymd.bottomsheet import MDListBottomSheet, MDGridBottomSheet
@@ -60,17 +63,22 @@ capture = cv2.VideoCapture(0)
 #######################################################################
 
 imageNames = os.listdir(imagesDir)
-#print("Found these images in users folder " + str(imageNames))
+# print("Found these images in users folder " + str(imageNames))
 for name in imageNames:
-	#print(face_recognition.face_encodings(face_recognition.load_image_file(imagesDir+name)))
-	known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(imagesDir+name))[0])
+	# print(face_recognition.face_encodings(face_recognition.load_image_file(imagesDir+name)))
+	known_face_encodings.append(face_recognition.face_encodings(face_recognition.load_image_file(imagesDir + name))[0])
 	known_face_names.append(name.split('.')[0])
+
+
 ############             Training end                       ############
 
 class IconRightSampleWidget(IRightBodyTouch, MDCheckbox):
 	pass
+
+
 class AvatarSampleWidget(ILeftBody, Image):
 	pass
+
 
 #######################################################################
 ## 	        Authenticating user                                      ##
@@ -81,7 +89,6 @@ class LoginScreen(Screen):
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		self.register_event_type('on_login')
-		
 	
 	def dostart(self):
 		try:
@@ -89,15 +96,15 @@ class LoginScreen(Screen):
 			self.ids.home_container.came.remove_widget(self.ids.cc)
 		except:
 			pass
-		bx = BoxLayout(orientation = "vertical", spacing = "10", padding = "10",id="bxla")
+		bx = BoxLayout(orientation="vertical", spacing="10", padding="10", id="bxla")
 		self.camauth = CameraAuth(id="cme")
 		self.camauth.bind(on_login_return=self.disp_home)
 		self.camauth.size = 0.8 * self.width, 0.6 * self.height
 		bx.add_widget(self.camauth)
 		self.ids.login_cam.add_widget(bx)
-		
+	
 	def dostop(self):
-		#self.remove_widget(self.ids.login_cam)
+		# self.remove_widget(self.ids.login_cam)
 		print(self.ids.login_cam.children)
 		self.camauth.stop()
 	
@@ -110,6 +117,7 @@ class LoginScreen(Screen):
 	def on_login(self):
 		pass
 
+
 class CameraAuth(Image):
 	
 	def __init__(self, **kwargs):
@@ -118,11 +126,10 @@ class CameraAuth(Image):
 		self.capture = None
 		global capture
 		self.start(capture)
-		
+	
 	def on_login_return(self):
 		pass
 	
-		
 	def start(self, capture, fps=30):
 		self.capture = capture
 		rect, frame = self.capture.read()
@@ -220,6 +227,7 @@ class CameraAuth(Image):
 class UserImagesButton(ListItemButton):
 	pass
 
+
 class ListViewModal(ModalView):
 	def __init__(self, **kwargs):
 		super(ListViewModal, self).__init__(**kwargs)
@@ -245,30 +253,30 @@ class UserImageDetailView(BoxLayout):
 		if not type(self.user_name) == type(a):
 			print("user name is " + self.user_name)
 			self.add_widget(Image(
-				source=imagesDir+self.user_name,
+				source=imagesDir + self.user_name,
 				size=(450, 450)))
-	
+			
 			## TODO display file attribs ##
 			if self.user_name == "[]":
 				return
-			user_attributes =os.stat(imagesDir + self.user_name)
+			user_attributes = os.stat(imagesDir + self.user_name)
 			fileProtectionMode = str(user_attributes.st_mode)
 			fileInode = str(user_attributes.st_ino)
-			fileSize = str(float(user_attributes.st_size)/1000) + " KB"
-			fileLastEdited = str(user_attributes.st_mtime)
-			fileLastAccessed = str(user_attributes.st_atime)
-			#print(type(os.stat(imagesDir + self.user_name)))
-			attrib_grid = GridLayout(rows=6, cols = 2, padding = 25, spacing = 25)
-			attrib_grid.add_widget(Label(text='User Name',color=[1, 1, 1, 0.8], halign = "left"))
-			attrib_grid.add_widget(Label(text=self.user_name.strip('.'),color=[1, 1, 1, 0.8], halign = "left"))
-			attrib_grid.add_widget(Label(text='File Size', color=[1, 1, 1, 0.8],  halign = "left"))
-			attrib_grid.add_widget(Label(text=fileSize,  color=[1, 1, 1, 0.8],  halign = "left"))
-			attrib_grid.add_widget(Label(text='last Accessed timestamp', color=[1, 1, 1, 0.8],  halign = "left"))
-			attrib_grid.add_widget(Label(text=fileLastAccessed, color=[1, 1, 1, 0.8],  halign = "left"))
-			attrib_grid.add_widget(Label(text='last Edited timestamp', color=[1, 1, 1, 0.8]))
-			attrib_grid.add_widget(Label(text=fileLastEdited,color=[1, 1, 1, 0.8]))
-			#attrib_grid.add_widget(Label(text='File Protection Mode', color=[1, 1, 1, 0.8]))
-			#attrib_grid.add_widget(Label(text=fileProtectionMode, color=[1, 1, 1, 0.8]))
+			fileSize = str(float(user_attributes.st_size) / 1000) + " KB"
+			#fileLastEdited = str(user_attributes.st_mtime)
+			#fileLastAccessed = str(user_attributes.st_atime)
+			# print(type(os.stat(imagesDir + self.user_name)))
+			attrib_grid = GridLayout(rows=6, cols=2, padding=25, spacing=25)
+			attrib_grid.add_widget(Label(text='User Name', color=[1, 1, 1, 0.8], halign="left"))
+			attrib_grid.add_widget(Label(text=self.user_name.strip('.'), color=[1, 1, 1, 0.8], halign="left"))
+			attrib_grid.add_widget(Label(text='File Size', color=[1, 1, 1, 0.8], halign="left"))
+			attrib_grid.add_widget(Label(text=fileSize, color=[1, 1, 1, 0.8], halign="left"))
+			#attrib_grid.add_widget(Label(text='last Accessed timestamp', color=[1, 1, 1, 0.8], halign="left"))
+			#attrib_grid.add_widget(Label(text=fileLastAccessed, color=[1, 1, 1, 0.8], halign="left"))
+			#attrib_grid.add_widget(Label(text='last Edited timestamp', color=[1, 1, 1, 0.8]))
+			#attrib_grid.add_widget(Label(text=fileLastEdited, color=[1, 1, 1, 0.8]))
+			# attrib_grid.add_widget(Label(text='File Protection Mode', color=[1, 1, 1, 0.8]))
+			# attrib_grid.add_widget(Label(text=fileProtectionMode, color=[1, 1, 1, 0.8]))
 			
 			self.add_widget(attrib_grid)
 	
@@ -284,10 +292,12 @@ class UserImageDetailView(BoxLayout):
 				self.user_name = selected_object.text
 		
 		self.redraw()
+	
 	def clearwids(self):
 		self.clear_widgets()
+
+
 class HomeScreen(Screen):
-	
 	users_list = ObjectProperty()
 	
 	def __init__(self, **kwargs):
@@ -298,9 +308,9 @@ class HomeScreen(Screen):
 		global auth_user_name
 		print("auth user name is ++++" + auth_user_name)
 		self.user_name_auth = auth_user_name
-		
+	
 	def setUserDirInfo(self):
-		#print(self.ids)
+		# print(self.ids)
 		self.userImages = os.listdir(imagesDir)
 		
 		for user in self.userImages:
@@ -318,108 +328,112 @@ class HomeScreen(Screen):
 		self.users_list.adapter.bind(
 			on_selection_change=self.detail_view.user_changed)
 		self.ids.detail_user.add_widget(self.detail_view)
-		
-		#print("Ids are -------------------\n" + str(self.ids))
+	
+	# print("Ids are -------------------\n" + str(self.ids))
 	
 	def delete_user(self):
-		#print(self)
-		#print(self.users_list.adapter.selection[0])
-		#print(type(self.users_list.adapter.selection[0]))
-		#print(self.users_list.adapter.selection[0].text)
+		# print(self)
+		# print(self.users_list.adapter.selection[0])
+		# print(type(self.users_list.adapter.selection[0]))
+		# print(self.users_list.adapter.selection[0].text)
 		self.user_delete = self.users_list.adapter.selection[0].text
 		self.user_name_delete = self.user_delete.split('.')[0]
 		self.content = content = MDLabel(font_style='Subhead',
-                          theme_text_color='Secondary',
-                          text="All data associated with " + self.user_name_delete + " has been removed successfully!",
-                          size_hint_y=None,
-                          valign='top')
+										 theme_text_color='Secondary',
+										 text="All data associated with " + self.user_name_delete + " has been removed successfully!",
+										 size_hint_y=None,
+										 valign='top')
 		self.dialog = MDDialog(title="Conform user delete operation",
 							   content=self.content,
 							   size_hint=(.8, None),
 							   height=dp(200),
 							   auto_dismiss=False)
 		
-		self.dialog.add_action_button("Dismiss",action=lambda *x: self.dialog.dismiss())
+		self.dialog.add_action_button("Dismiss", action=lambda *x: self.dialog.dismiss())
 		self.dialog.open()
 		print("########################")
 		print(self.ids)
 		self.detail_view.clearwids()
 		self.ids.detail_user.clear_widgets()
 		os.remove(imagesDir + self.user_delete)
-		#print("delete called")
+		# print("delete called")
 		self.users_list.adapter.data = []
 		self.setUserDirInfo()
 		self.detail_view.redraw()
 	
 	def edit_user(self):
-		#print(self)
-		#rint(self.users_list.adapter.selection[0].split('=')[1])
+		# print(self)
+		# rint(self.users_list.adapter.selection[0].split('=')[1])
 		print("edit user called")
-		
+	
 	def on_newuser(self):
 		pass
-	def on_logout(self, **kwargs):
-		#print(kwargs)
-		pass
 	
+	def on_logout(self, **kwargs):
+		# print(kwargs)
+		pass
+
+
 class UserImages(MDList):
 	user_list = ObjectProperty()
+	
 	def __init__(self, **kwargs):
 		super().__init__(**kwargs)
 		print("called user image class")
-		#print(self.ids)
+		# print(self.ids)
 		print(scrmgr)
 		root = scrmgr
-		#print(dir(root))
-		#self.cols = 3
-		#self.row_default_height = (root.width - self.cols * self.spacing[0]) / self.cols
+		# print(dir(root))
+		# self.cols = 3
+		# self.row_default_height = (root.width - self.cols * self.spacing[0]) / self.cols
 		
 		self.userImages = os.listdir(imagesDir)
-		#heading = MDLabel()
-		#heading.font_name = 'Lato-Regular.ttf'
-		#heading.text = "Home Screen."
-		#heading.halign = 'center'
-		#heading.font_size ="20dp"
-		#heading.size = 800 , 25
-		#self.add_widget(heading)
-		#intro = MDLabel()
-		#intro.font_name = 'Lato-Regular.ttf'
-		#intro.text = "Here is the list of known users. Please note all actions performed here can not be reversed."
-		#intro.font_size = "18dp"
-		#self.add_widget(intro)
+		# heading = MDLabel()
+		# heading.font_name = 'Lato-Regular.ttf'
+		# heading.text = "Home Screen."
+		# heading.halign = 'center'
+		# heading.font_size ="20dp"
+		# heading.size = 800 , 25
+		# self.add_widget(heading)
+		# intro = MDLabel()
+		# intro.font_name = 'Lato-Regular.ttf'
+		# intro.text = "Here is the list of known users. Please note all actions performed here can not be reversed."
+		# intro.font_size = "18dp"
+		# self.add_widget(intro)
 		
-		#print("found images ", self.userImages )
+		# print("found images ", self.userImages )
 		
 		for user in self.userImages:
 			# print(face_recognition.face_encodings(face_recognition.load_image_file(imagesDir+name)))
-			#print("adding " + user)
+			# print("adding " + user)
 			rightIcon = IconRightSampleWidget()
-			avatar = AvatarSampleWidget(source = imagesDir+user)
-			#print(os.stat(imagesDir + user))
-			tempwid = TwoLineAvatarIconListItem(text = user, secondary_text = "...with avatar&icon")
+			avatar = AvatarSampleWidget(source=imagesDir + user)
+			# print(os.stat(imagesDir + user))
+			tempwid = TwoLineAvatarIconListItem(text=user, secondary_text="...with avatar&icon")
 			tempwid.id = user
 			tempwid.add_widget(avatar)
 			tempwid.add_widget(rightIcon)
-			tempwid.on_release=self.editUser
+			tempwid.on_release = self.editUser
 			self.add_widget(tempwid)
-			#self.add_widget(SmartTile(mipmap=True, source=imagesDir + user))
-			#pass
+	
+	# self.add_widget(SmartTile(mipmap=True, source=imagesDir + user))
+	# pass
 	
 	def on_login(self):
 		print("disp @ loginScrn clasx	")
 		pass
-
+	
 	def editUser(self):
-		
-		#print("clicked!" + str(self))
-		#print(self.ids)
+		# print("clicked!" + str(self))
+		# print(self.ids)
 		bs = MDListBottomSheet()
 		bs.add_item("Change user details of ", lambda x: x)
 		bs.add_item("Here's an item with an icon", lambda x: x,
 					icon='clipboard-account')
 		bs.add_item("Here's another!", lambda x: x, icon='nfc')
 		bs.open()
-	
+
+
 #######################################################################
 ## 	        Regestration of user                                     ##
 #######################################################################
@@ -428,12 +442,14 @@ class KivyCameraReg(Image):
 	def __init__(self, **kwargs):
 		super(KivyCameraReg, self).__init__(**kwargs)
 		self.capture = None
-		#self.start(capture)
+	
+	# self.start(capture)
 	
 	def start(self, capture, fps=30):
 		self.capture = capture
 		Clock.schedule_interval(self.update, 1.0 / fps)
-		#print("start over reg")
+	
+	# print("start over reg")
 	
 	def stop(self):
 		Clock.unschedule(self.update)
@@ -449,7 +465,7 @@ class KivyCameraReg(Image):
 		
 		# Find all the faces and face encodings in the current frame of video
 		face_locations = face_recognition.face_locations(rgb_small_frame)
-		#print("Found face @ these locations: ", face_locations)
+		# print("Found face @ these locations: ", face_locations)
 		
 		# Display the results
 		drawColor = blueColor
@@ -462,13 +478,13 @@ class KivyCameraReg(Image):
 			left *= 4
 			
 			# Draw a box around the face
-			cv2.rectangle(frame, (left-100, top-100), (right+100, bottom+100), drawColor, 2)
+			cv2.rectangle(frame, (left - 100, top - 100), (right + 100, bottom + 100), drawColor, 2)
 			
 			# Draw a label with a name below the face
-			cv2.rectangle(frame, (left-100, bottom+100 ), (right-100, bottom+100), drawColor, cv2.FILLED)
+			cv2.rectangle(frame, (left - 100, bottom + 100), (right - 100, bottom + 100), drawColor, cv2.FILLED)
 			font = cv2.FONT_HERSHEY_DUPLEX
-			cv2.rectangle(frame, (left-100, bottom + 100), (right+100, bottom+150), drawColor, cv2.FILLED)
-			cv2.putText(frame, name, (left +95, bottom + 130), font, 1.0, (255, 255, 255), 1)
+			cv2.rectangle(frame, (left - 100, bottom + 100), (right + 100, bottom + 150), drawColor, cv2.FILLED)
+			cv2.putText(frame, name, (left + 95, bottom + 130), font, 1.0, (255, 255, 255), 1)
 		
 		# display frame
 		frame = cv2.flip(frame, 0)
@@ -489,7 +505,8 @@ class RegisterScreen(Screen):
 		super().__init__(**kwargs)
 		print("############REG#####REG##########")
 		self.capture = capture
-		#self.dostart(self.capture)
+	
+	# self.dostart(self.capture)
 	
 	def __enter__(self):
 		print("reg start req")
@@ -516,8 +533,8 @@ class RegisterScreen(Screen):
 		cv2.imwrite(imagesDir + username + ".png", frame)
 		print("Captured")
 	
-	def find_face(self,file_name):
-		frame = cv2.imread("./users/"+file_name+".png")
+	def find_face(self, file_name):
+		frame = cv2.imread("./users/" + file_name + ".png")
 		small_frame = cv2.resize(frame, (0, 0), fx=0.25, fy=0.25)
 		# Convert the image from BGR color to RGB color
 		rgb_small_frame = small_frame[:, :, ::-1]
@@ -527,7 +544,8 @@ class RegisterScreen(Screen):
 		# print("Found face @ these locations: ", face_locations)
 		if face_locations:
 			return True
-		return  False
+		return False
+
 
 #######################################################################
 ##                    Root FaceLog app                               ##
@@ -561,7 +579,7 @@ class FaceLogApp(App):
 		self.screenmanager = ScreenManager(transition=SwapTransition())
 		global scrmgr
 		scrmgr = self.screenmanager
-		#print("from build " + str(scrmgr))
+		# print("from build " + str(scrmgr))
 		self.loginscreen = LoginScreen(name='screen-login')
 		self.loginscreen.bind(on_login=self.login)
 		self.homescreen = HomeScreen(name='screen-home')
@@ -570,18 +588,17 @@ class FaceLogApp(App):
 		self.homescreen.bind(on_logout=self.logout_redirect)
 		self.screenmanager.add_widget(self.loginscreen)
 		self.screenmanager.add_widget(self.homescreen)
-		self.registerscreen = RegisterScreen(name = 'screen-register')
+		self.registerscreen = RegisterScreen(name='screen-register')
 		self.screenmanager.add_widget(self.registerscreen)
 		scrmgr = self.screenmanager
-		#self.screenmanager.current = 'screen-home'
+		# self.screenmanager.current = 'screen-home'
 		print(scrmgr)
 		inspector.create_inspector(Window, Button)
-	
-	
+
 	def newuser_redirect(self, *args):
 		print("new user redirect from main")
 		self.screenmanager.current = 'screen-register'
-		
+	
 	def logout_redirect(self, *args):
 		print("called from main redirect to login")
 		print("Logged in waiting for re-direction---")
@@ -589,14 +606,14 @@ class FaceLogApp(App):
 		print(self.screenmanager)
 		self.screenmanager.current = 'screen-login'
 		print(self.screenmanager.current)
-		
+	
 	def login(self, *args):
 		print("Logged in waiting for re-direction---")
 		print("recired args " + str(args))
 		self.screenmanager.current = 'screen-home'
 		print("auth user name is ++++" + auth_user_name)
 		self.loginSuccessDialog()
-		
+	
 	def loginSuccessDialog(self):
 		global auth_user_name
 		content = MDLabel(font_style='Body1',
@@ -615,11 +632,11 @@ class FaceLogApp(App):
 		self.dialog.add_action_button("Dismiss",
 									  action=lambda *x: self.dialog.dismiss())
 		self.dialog.open()
-		
+
+
 ############             root class end                   ############
 
 
-
 if __name__ == '__main__':
-	#creating instance to run
+	# creating instance to run
 	FaceLogApp().run()
